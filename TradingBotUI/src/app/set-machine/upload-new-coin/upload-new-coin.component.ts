3c30eb06817ea11f609e2ec2567996cf5686b679
Maksym Zimak <mzimak18@gmail.com>
2024-04-21 10:51:23 -0400

    Added uploading

--- TradingBotUI/src/app/set-machine/upload-new-coin/upload-new-coin.component.ts
+++ TradingBotUI/src/app/set-machine/upload-new-coin/upload-new-coin.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { IconService } from '../../../services/image-manager.service';
 
 @Component({
   selector: 'app-upload-new-coin',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './upload-new-coin.component.html',
   styleUrl: './upload-new-coin.component.scss'
 })
-export class UploadNewCoinComponent {
+export class UploadNewCoinComponent implements OnInit, OnDestroy {
+  constructor() {}
+  
+  ngOnInit() {
+    
+  }
 
+  ngOnDestroy() {
+    // Cleanup code here
+  }
+
+  public onFileSelected(event: any): void {
+    // Code to handle the selected file
+    const file: File = event.target.files[0];
+    // Further processing of the file
+  }
 }
